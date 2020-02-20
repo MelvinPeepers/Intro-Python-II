49e746501dff5b46d8f7b84494118f1131b56472
Melvin <peepersm@gmail.com>
2020-02-19 21:36:33 -0500

    added attributes to room.py

--- src/player.py
+++ src/player.py
@@ -1,5 +1,8 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+# https://micropyramid.com/blog/understand-self-and-__init__-method-in-python-class/
+
+
 from room import Room
 
 
