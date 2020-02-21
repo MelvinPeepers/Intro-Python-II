995db64c9d7dc78a3e0dbcedefc844bbaf053fa7
Melvin <peepersm@gmail.com>
2020-02-20 22:45:18 -0500

    added item class

--- src/item.py
+++ src/item.py
@@ -2,3 +2,12 @@ class Item:
     def __init__(self, name, description):
         self.name = name
         self.description = description
+
+    def __str__(self):
+        output = f' You see a {self.description} {self.name}.'
+
+        return output
+
+
+i = Item("rock", "big")
+print(i)
